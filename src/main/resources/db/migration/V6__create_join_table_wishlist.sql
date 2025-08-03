e79e427c7d3a2c40b4ded6b673e150524c4065ff
leonel ka <waboleonel@gmail.com>
2025-08-03 20:54:28 +0100

    Wishlist table creation migration

--- src/main/resources/db/migration/V6__create_join_table_wishlist.sql
+++ src/main/resources/db/migration/V6__create_join_table_wishlist.sql
@@ -1,8 +1,9 @@
-create table wishlist (
-    user_id bigint not null,
-    product_id bigint not null,
-    primary key(user_id, product_id),
-    key FK_products_user_wishlist (product_id),
-    constraint FK_user_wishlist foreign key (user_id) references users(id) on delete cascade,
-    constraint FK_product_wishlist foreign key (product_id) references products(id) on delete cascade
+CREATE TABLE wishlists (
+    user_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, product_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
+    KEY IDX_wishlist_product_id (product_id)
 );
