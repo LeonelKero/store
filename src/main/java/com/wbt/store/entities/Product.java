74352abfbc04cf3a52dcf926f146e7e4b712cc3d
leonel ka <waboleonel@gmail.com>
2025-08-03 20:49:54 +0100

    Define relationship between product and cart

--- src/main/java/com/wbt/store/entities/Product.java
+++ src/main/java/com/wbt/store/entities/Product.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Getter
@@ -36,4 +38,8 @@ public class Product {
     @ManyToMany(mappedBy = "wishlist")
     @Builder.Default
     private Set<User> users = new HashSet<>();
+
+    @OneToMany(mappedBy = "product")
+    @JoinColumn(name = "card_item_id")
+    private List<CartItem> cartItems = new ArrayList<>();
 }
