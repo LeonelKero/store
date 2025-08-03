abbefb1d975c79ced3457cb0b9e909885a74eb41
leonel ka <waboleonel@gmail.com>
2025-08-03 20:52:06 +0100

    Code refactoring, rename unique key

--- src/main/resources/db/migration/V1__create_table_users.sql
+++ src/main/resources/db/migration/V1__create_table_users.sql
@@ -1,8 +1,8 @@
-create TABLE users (
-  id bigint NOT NULL AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
-  password varchar(255) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY UK_6dotkott2kjsp8vw4d0m25fb7 (email)
+CREATE TABLE users (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY UK_user_email (email)
 );
