fb501e2eb9d4fbad6b0c5a524b2e545d131bf87a
leonel ka <waboleonel@gmail.com>
2025-08-03 21:11:17 +0100

    Code refactoring

--- src/main/java/com/wbt/store/services/ProductService.java
+++ src/main/java/com/wbt/store/services/ProductService.java
@@ -1,10 +1,19 @@
 package com.wbt.store.services;
 
 import com.wbt.store.entities.Product;
+import org.springframework.data.domain.Page;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface ProductService {
     List<Product> getAllProducts();
+
     List<Product> fetchProductByCriteria();
+
+    List<Product> findProductsBySpecification(String name, BigDecimal minPrice, BigDecimal maxPrice);
+
+    List<Product> sortedProductByName();
+
+    Page<Product> fetchPagedProducts(Integer page, Integer size);
 }
--- src/main/java/com/wbt/store/services/impl/ProductServiceImpl.java
+++ src/main/java/com/wbt/store/services/impl/ProductServiceImpl.java
@@ -2,10 +2,11 @@ package com.wbt.store.services.impl;
 
 import com.wbt.store.entities.Product;
 import com.wbt.store.repositories.ProductRepository;
+import com.wbt.store.repositories.specifications.ProductSpec;
 import com.wbt.store.services.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -35,5 +36,27 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public List<Product> fetchProductByCriteria() {
         return this.repository.findProductsByCriteria(null, BigDecimal.valueOf(1), BigDecimal.valueOf(10));
+    }
+
+
+    @Override
+    public List<Product> findProductsBySpecification(final String name, final BigDecimal minPrice, final BigDecimal maxPrice) {
+        Specification<Product> specification = Specification.where(null);
+        if (name != null) specification.and(ProductSpec.hasName(name));
+        if (minPrice != null) specification.and(ProductSpec.hasPriceEqualOrGreaterThen(minPrice));
+        if (maxPrice != null) specification.and(ProductSpec.hasPriceLessThanOrEqualTo(maxPrice));
+        return this.repository.findAll(specification);
+    }
+
+    @Override
+    public List<Product> sortedProductByName() {
+        Sort sort = Sort.by("name")
+                .and(Sort.by("price").descending());
+        return this.repository.findAll(sort);
+    }
+
+    @Override
+    public Page<Product> fetchPagedProducts(final Integer page, final Integer size) {
+        return this.repository.findAll(PageRequest.of(page, size));
     }
 }
