9ea206d14aa1ee9145680345c9fb60086e358720
leonel ka <waboleonel@gmail.com>
2025-08-03 20:52:58 +0100

    Code refactoring, cleanup

--- src/main/resources/db/migration/V3__create_table_addresses.sql
+++ src/main/resources/db/migration/V3__create_table_addresses.sql
@@ -1,9 +1,7 @@
-create TABLE addresses (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  city VARCHAR(255) NOT NULL,
-  street VARCHAR(255) NOT NULL,
-  user_id BIGINT,
-  PRIMARY KEY (id),
-  KEY FK_users_addresses (user_id),
-  CONSTRAINT FK_users_addresses FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+CREATE TABLE addresses (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    city VARCHAR(255) NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    user_id BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
