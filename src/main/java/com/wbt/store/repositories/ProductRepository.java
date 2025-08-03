a881158eb28c696b268aa0110c7fe3286810c93b
leonel ka <waboleonel@gmail.com>
2025-08-03 11:33:10 +0100

    Update product repository

--- src/main/java/com/wbt/store/repositories/ProductRepository.java
+++ src/main/java/com/wbt/store/repositories/ProductRepository.java
@@ -3,8 +3,10 @@ package com.wbt.store.repositories;
 import com.wbt.store.dtos.ProductSummaryDTO;
 import com.wbt.store.entities.Category;
 import com.wbt.store.entities.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -13,5 +15,11 @@ public interface ProductRepository extends JpaRepository<Product, Long>, Product
     // List<ProductSummary> findByCategory(Category category);
 
     // Another projection
-    List<ProductSummaryDTO> findByCategory(Category category);
+    // List<ProductSummaryDTO> findByCategory(Category category);
+
+    @EntityGraph(attributePaths = {"category"})
+    @Query("select p from Product p")
+    List<Product> findByProductsWithCategory();
+
+    List<Product> findByCategory_IdOrderByPriceAsc(Byte id);
 }
