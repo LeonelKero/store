c20a903e7bea3ecb480eb4e58b4f191f0f2c4598
leonel ka <waboleonel@gmail.com>
2025-08-03 20:49:16 +0100

    Create cart item entity

---
+++ src/main/java/com/wbt/store/entities/CartItem.java
@@ -0,0 +1,31 @@
+package com.wbt.store.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "cart_item",
+        uniqueConstraints = @UniqueConstraint(
+                columnNames = {"cart_id", "product_id"}
+        ))
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+}
