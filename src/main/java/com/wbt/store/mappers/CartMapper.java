acbdf9206e37c81379511cc6f2ec5c879299fe00
leonel ka <waboleonel@gmail.com>
2025-08-10 17:00:10 +0100

    Cart mapper

---
+++ src/main/java/com/wbt/store/mappers/CartMapper.java
@@ -0,0 +1,25 @@
+package com.wbt.store.mappers;
+
+import com.wbt.store.dtos.CartDto;
+import com.wbt.store.dtos.CartItemDto;
+import com.wbt.store.dtos.ProductItemDto;
+import com.wbt.store.entities.Cart;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class CartMapper {
+
+    public CartDto toCartDto(final Cart cart) {
+        return new CartDto(
+                cart.getId(),
+                cart.getItems().stream().map(item -> new CartItemDto(
+                        new ProductItemDto(item.getProduct().getId(), item.getProduct().getName(), item.getProduct().getPrice()),
+                        item.getQuantity(),
+                        item.getTotalPrice()
+                )).collect(Collectors.toSet()),
+                cart.calculatePrice());
+    }
+
+}
