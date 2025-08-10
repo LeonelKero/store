6f99aec92abdd37cac2238d73f2e9eb747f71ea8
leonel ka <waboleonel@gmail.com>
2025-08-10 16:59:57 +0100

    Fetch cart item by cart id

--- src/main/java/com/wbt/store/repositories/CartItemRepository.java
+++ src/main/java/com/wbt/store/repositories/CartItemRepository.java
@@ -3,5 +3,8 @@ package com.wbt.store.repositories;
 import com.wbt.store.entities.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    CartItem findByCart_Id(UUID id);
 }
