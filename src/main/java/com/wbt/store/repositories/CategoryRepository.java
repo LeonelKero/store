1ebd6c4fe2c0f0e83ebc4acedc6c5cd6cebc37d0
leonel ka <waboleonel@gmail.com>
2025-08-11 22:19:19 +0100

    Add custom method into category repository

--- src/main/java/com/wbt/store/repositories/CategoryRepository.java
+++ src/main/java/com/wbt/store/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.wbt.store.repositories;
 import com.wbt.store.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Byte> {
+
+    Optional<Category> findByNameIgnoreCase(String name);
 }
