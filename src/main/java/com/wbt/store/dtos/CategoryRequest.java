40bd903add53290fc94dd8f746f2a8d36d51d4bf
leonel ka <waboleonel@gmail.com>
2025-08-16 12:08:47 +0100

    Fix miss validation

--- src/main/java/com/wbt/store/dtos/CategoryRequest.java
+++ src/main/java/com/wbt/store/dtos/CategoryRequest.java
@@ -1,13 +1,11 @@
 package com.wbt.store.dtos;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CategoryRequest(
         @NotBlank(message = "Category name is required")
-        @Min(value = 2, message = "At least 02 characters for name required")
-        @Max(value = 20, message = "Maximum of 20 characters for category name")
+        @Size(min = 2, max = 20, message = "Name must be between 2-20 characters")
         String name
 ) {
 }
