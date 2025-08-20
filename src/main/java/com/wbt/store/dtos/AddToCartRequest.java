b65fbd8c104317accc90a0b8d0ab34c14744436c
leonel ka <waboleonel@gmail.com>
2025-08-20 20:43:12 +0100

    Update cart add request DTO

--- src/main/java/com/wbt/store/dtos/AddToCartRequest.java
+++ src/main/java/com/wbt/store/dtos/AddToCartRequest.java
@@ -1,9 +1,13 @@
 package com.wbt.store.dtos;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record AddToCartRequest(
-        @NotBlank(message = "Missing product Id")
+        @NotNull(message = "The product Id is required")
+        @Positive(message = "Product ID must be a positive value")
+        @Min(value = 1, message = "Id cannot be negative")
         Long productId
 ) {
 }
