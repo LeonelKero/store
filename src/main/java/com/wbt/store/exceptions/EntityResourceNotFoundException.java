73e76dec6ef31c3c71f6e0140fdc75ba0e7ad7b4
leonel ka <waboleonel@gmail.com>
2025-08-28 20:35:01 +0100

    Defining exception for entity not found

---
+++ src/main/java/com/wbt/store/exceptions/EntityResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.wbt.store.exceptions;
+
+public class EntityResourceNotFoundException extends RuntimeException{
+    public EntityResourceNotFoundException(String message) {
+        super(message);
+    }
+}
